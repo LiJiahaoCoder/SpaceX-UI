e37323e892cc70f01d455bde14760c21d1378125
Jiahao Li <lijiahao_shandong@163.com>
2020-06-20 10:51:42 +0800

    add active style and extract center mixin

--- src/components/controlRod/index.tsx
+++ src/components/controlRod/index.tsx
@@ -9,7 +9,7 @@ export default function ControlRod () {
     <div className={`${styles.btn} ${styles['ver-btn']} ${styles.right}`}><RightArrow /></div>
     <div className={`${styles.btn} ${styles['hor-btn']} ${styles.bottom}`}><DownArrow /></div>
     <div className={`${styles.btn} ${styles['ver-btn']} ${styles.left}`}><LeftArrow /></div>
-    <div className={styles['toggle-out']}></div>
-    <div className={styles['toggle-inner']}></div>
+    <div className={`${styles.btn} ${styles['toggle-out']}`}></div>
+    <div className={`${styles.btn} ${styles['toggle-inner']}`}></div>
   </div>;
 }
